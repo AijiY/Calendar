38373eb4c1017ed6e75c2a62e312bf13df56ea6f
Aiji Yamamura <j2yamimjr3i@gmail.com>
2024-08-11 15:54:01 +0900

    横スワイプ機能追加

--- app/src/main/java/com/example/mytodo/DayFragment.java
+++ app/src/main/java/com/example/mytodo/DayFragment.java
@@ -4,23 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import java.util.Date;
 
 public class DayFragment extends Fragment {
+  private static final String ARG_DATE = "showingDate";
+  private Date showingDate;
+
   public DayFragment() {
   }
-  public static DayFragment newInstance() {
+
+  public static DayFragment newInstance(Date showingDate) {
     DayFragment fragment = new DayFragment();
+    Bundle args = new Bundle();
+    args.putSerializable(ARG_DATE, showingDate);
+    fragment.setArguments(args);
     return fragment;
   }
+
   @Override
-  public void onCreate(Bundle savedInstanceState) {
+  public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    if (getArguments() != null) {
+      showingDate = (Date) getArguments().getSerializable(ARG_DATE);
+    }
   }
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
+
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     return inflater.inflate(R.layout.fragment_day, container, false);
   }
 }
