0e40a2dccf716d09a71a5f26bfab3fce9e695787
Aiji Yamamura <j2yamimjr3i@gmail.com>
2024-08-12 19:49:25 +0900

    ・add画面途中

---
+++ app/src/main/java/com/example/mytodo/CreateToDoActivity.java
@@ -0,0 +1,87 @@
+package com.example.mytodo;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class CreateToDoActivity extends AppCompatActivity {
+  private EditText editStartDate;
+  private EditText editEndDate;
+  private EditText editStartTime;
+  private EditText editEndTime;
+
+  private Calendar calendarStart;
+  private Calendar calendarEnd;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_create_todo);
+
+    editStartDate = findViewById(R.id.editStartDate);
+    editEndDate = findViewById(R.id.editEndDate);
+    editStartTime = findViewById(R.id.editStartTime);
+    editEndTime = findViewById(R.id.editEndTime);
+
+//    初期値設定
+    Date showingDate = MainActivity.showingDate;
+    calendarStart = Calendar.getInstance();
+    calendarStart.setTime(showingDate);
+    calendarEnd = Calendar.getInstance();
+    calendarEnd.setTime(showingDate);
+
+    calendarStart.set(Calendar.MINUTE, 0);
+    calendarStart.set(Calendar.SECOND, 0);
+    calendarStart.set(Calendar.MILLISECOND, 0);
+
+    calendarEnd.set(Calendar.MINUTE, 0);
+    calendarEnd.set(Calendar.SECOND, 0);
+    calendarEnd.set(Calendar.MILLISECOND, 0);
+    calendarEnd.add(Calendar.HOUR_OF_DAY, 1);
+
+    updateDateLabel(editStartDate, calendarStart);
+    updateDateLabel(editEndDate, calendarEnd);
+    updateTimeLabel(editStartTime, calendarStart);
+    updateTimeLabel(editEndTime, calendarEnd);
+
+
+//    時間編集イベント
+    editStartDate.setOnClickListener(v -> showDatePicker(calendarStart, editStartDate));
+    editEndDate.setOnClickListener(v -> showDatePicker(calendarEnd, editEndDate));
+    editStartTime.setOnClickListener(v -> showTimePicker(calendarStart, editStartTime));
+    editEndTime.setOnClickListener(v -> showTimePicker(calendarEnd, editEndTime));
+
+  }
+
+  private void showDatePicker(Calendar calendar, EditText editText) {
+    new DatePickerDialog(CreateToDoActivity.this, (view, year, month, dayOfMonth) -> {
+      calendar.set(year, month, dayOfMonth);
+      updateDateLabel(editText, calendar);
+    }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+  }
+
+  private void showTimePicker(Calendar calendar, EditText editText) {
+    new TimePickerDialog(CreateToDoActivity.this, (view, hourOfDay, minute) -> {
+      calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+      calendar.set(Calendar.MINUTE, minute);
+      updateTimeLabel(editText, calendar);
+    }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show();
+  }
+
+  private void updateDateLabel(EditText editText, Calendar calendar) {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd EEEE", Locale.getDefault());
+    editText.setText(sdf.format(calendar.getTime()));
+  }
+
+  private void updateTimeLabel(EditText editText, Calendar calendar) {
+    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.getDefault());
+    editText.setText(sdf.format(calendar.getTime()));
+  }
+
+}
